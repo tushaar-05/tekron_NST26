59f00cab234630d0facbf8ce61b0e88e663c2930
Prabhav1437 <jainprabhav1437@gmail.com>
2025-12-10 23:06:56 +0530

    meow

---
+++ src/data/eventsData.js
@@ -0,0 +1,23 @@
+export const competitions = [
+    { id: 1, title: 'Trade X', category: 'Competition' },
+    { id: 2, title: 'CodeWars', category: 'Competition' },
+    { id: 3, title: 'Drone Havoc', category: 'Competition' },
+    { id: 4, title: 'Robo Kick', category: 'Competition' },
+    { id: 5, title: 'Hackron', category: 'Competition' },
+    { id: 6, title: 'Battle of Bands', category: 'Competition' },
+    { id: 7, title: 'Catch The Flag', category: 'Competition' },
+    { id: 8, title: 'Photography', category: 'Competition' },
+    { id: 9, title: 'BGMI', category: 'Competition' },
+    { id: 10, title: 'Throttle X', category: 'Competition' },
+];
+
+export const events = [
+    { id: 1, title: 'TedX', category: 'Event' },
+    { id: 2, title: 'Moto Expo', category: 'Event' },
+    { id: 3, title: 'Drone Show', category: 'Event' },
+    { id: 4, title: 'Fun Activities', category: 'Event' },
+    { id: 5, title: 'AR&VR , Metabat', category: 'Event' },
+    { id: 6, title: 'Game Dev', category: 'Workshop' },
+    { id: 7, title: 'Robotics', category: 'Workshop' },
+    { id: 8, title: 'Photography', category: 'Workshop' },
+];
